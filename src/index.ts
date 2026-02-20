3c20a92fa8a022da25d24ec2b2595a5b8861ba7a
Peixotim <pedropeixotovz@gmail.com>
2026-02-20 08:46:18 -0300

    feat: Adiciona middlewares de tratamento de erros e not found ao servidor

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,21 @@ import express from 'express';
 import { configDotenv } from 'dotenv';
 import { routes } from './routes/routes';
 import { Request,Response } from 'express';
+import { notFoundMiddleware } from './middlewares/not-found.middleware';
+import { errorHandlerMiddleware } from './middlewares/error-handler.middleware';
+
 configDotenv({
-  path:'.env'
+  path: '.env'
 });
 
 const app = express();
 const port = process.env.PORT;
+
 app.use(express.json())
 app.use(routes)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 
 app.get('/health', (req: Request, res : Response) =>{
   console.log(`API is running`)
